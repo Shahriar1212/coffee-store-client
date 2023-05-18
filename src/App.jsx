a34adc3e9fa96ef5b340216d1583eae82025cb8b
Shahriar1212 <shahriarmoin.aust@gmail.com>
2023-05-18 10:43:21 +0600

    create and read coffee

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { useState } from 'react';
 import './App.css'
+import { useLoaderData } from 'react-router-dom'
+import CoffeeCard from './components/CoffeeCard';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const loadedCoffees = useLoaderData();
+
+  const [coffees, setCoffees] = useState(loadedCoffees);
 
   return (
-    <>
-
-      <h1 className='text-6xl text-purple-600'>Vite + React</h1>
-      
-    </>
+    <div className='m-20'>
+      <h1 className='text-6xl text-center my-20 text-purple-600'>Hot Hot Cold Coffee: {coffees.length}</h1>
+      <div className='grid md:grid-cols-2 gap-4'>
+        {
+          coffees.map(coffee => <CoffeeCard
+            key={coffee._id}
+            coffee={coffee}
+            coffees={coffees}
+            setCoffees={setCoffees}
+          ></CoffeeCard>)
+        }
+      </div>
+    </div>
   )
 }
 
